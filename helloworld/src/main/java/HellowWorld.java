1ebf807a750aa3c804ce23bb3d470db29769a95c
linzeyu1993 <linzeyu1993@163.com>
2021-05-28 00:07:00 +0800

    创建hello工程

---
+++ helloworld/src/main/java/HellowWorld.java
@@ -0,0 +1,9 @@
+public class HellowWorld {
+    public void test(){
+        System.out.println("test");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("start");
+    }
+}
